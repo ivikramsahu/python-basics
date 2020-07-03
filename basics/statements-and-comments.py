f90f176e327c702452a63fdab454b12b633fcf65
Vikram <snipperbytes@gmail.com>
2020-07-03 23:10:18 +0530

    adding multiline # comments

---
+++ basics/statements-and-comments.py
@@ -0,0 +1,30 @@
+# Multiline statements 
+
+a = 1 + 2 + \
+    3 + 5 + \
+    8 + 7 
+
+print (a)
+            
+# OR 
+
+b = ( 1 + 2 +
+     3 + 6 +
+     8 + 9
+    )
+
+print (b)
+
+# OR 
+
+colors = ['red',
+          'blue',
+          'green']
+
+print (colors)
+
+# OR
+
+name = 1; surname = 2; value = 3
+
+print(name , " + " , surname , " " + ":" , value)
