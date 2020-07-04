f5fdfea963245334d5c094ffbcc230144a782981
Vikram <snipperbytes@gmail.com>
2020-07-04 18:24:50 +0530

    about variables

---
+++ basics/variables.py
@@ -0,0 +1,23 @@
+"""
+ variable are used to store data in memory which can be later updated.
+ Assigning values to variables
+"""
+
+name = "vikram"
+print("Before re-assigning " + name)
+name = 1
+print("After re-assigning ", name)
+
+# Multiple assignments
+
+x, y, z = 12, 10.4, "Sahu"
+
+print(x, y, z)
+
+# Assigning Constant values (use capital letters possible)
+
+PI = 3.14
+ADMIN = "snipperbytes"
+
+print(PI)
+print(ADMIN)
