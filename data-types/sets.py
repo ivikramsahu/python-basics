7b38bcee8a544c8e4c726f8c05841fa0722855d7
Vikram <snipperbytes@gmail.com>
2020-07-06 12:28:30 +0530

    a unordered and unindexed

---
+++ data-types/sets.py
@@ -0,0 +1,25 @@
+'''
+Python Set
+Set is an unordered collection of unique items
+Denoted by {}
+'''
+a = {5, 2, 3, 1, 4}
+
+# printing set variable
+print("a = ", a)
+
+# data type of variable a
+print(type(a))
+
+''' 
+what can be output of a[1] run and check.. ?
+.
+.
+.
+.
+.
+.
+.
+.
+Since, set are unordered collection, indexing has no meaning. Hence, the slicing operator [] does not work.
+'''
