6be94b544ee211680c6ef50c2f5d398efa0d9c63
Vikram <snipperbytes@gmail.com>
2020-07-06 14:05:37 +0530

    scope of variable in func

---
+++ functions/scope-life-in-function.py
@@ -0,0 +1,10 @@
+# Scope and Lifetime of variables with respective function
+
+def my_func():
+    x = 10
+    print("Value inside function:", x)
+
+
+x = 20
+my_func()
+print("Value outside function:", x)
