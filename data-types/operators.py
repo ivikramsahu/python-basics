f0bb13d510f2555477dcb8065336c82aa4903c6e
Vikram <snipperbytes@gmail.com>
2020-07-06 13:21:46 +0530

    adding operators

---
+++ data-types/operators.py
@@ -0,0 +1,78 @@
+# Operators are special symbols in Python that carry out arithmetic or logical computation.
+# Arithmetic operators
+
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =', x+y)
+
+# Output: x - y = 11
+print('x - y =', x-y)
+
+# Output: x * y = 60
+print('x * y =', x*y)
+
+# Output: x / y = 3.75
+print('x / y =', x/y)
+
+# Output: x // y = 3
+print('x // y =', x//y)
+
+# Output: x ** y = 50625
+print('x ** y =', x**y)
+
+#  Comparison operators [True or False]
+
+x = 10
+y = 12
+
+# Output: x > y is False
+print('x > y is', x > y)
+
+# Output: x < y is True
+print('x < y is', x < y)
+
+# Output: x == y is False
+print('x == y is', x == y)
+
+# Output: x != y is True
+print('x != y is', x != y)
+
+# Output: x >= y is False
+print('x >= y is', x >= y)
+
+# Output: x <= y is True
+print('x <= y is', x <= y)
+
+# Logical operators [and, or, not]
+
+x = True
+y = False
+
+print('x and y is', x and y)
+
+print('x or y is', x or y)
+
+print('not x is', not x)
+
+# Bitwise operators
+
+'''
+x= 10 and y = 4
+Operator	Meaning	            Example
+---------------------------------------------------------
+&	        Bitwise AND	        x & y = 0 (0000 0000)
+|	        Bitwise OR	        x | y = 14 (0000 1110)
+~	        Bitwise NOT	        ~x = -11 (1111 0101)
+^	        Bitwise XOR	        x ^ y = 14 (0000 1110)
+>>	        Bitwise right shift	x >> 2 = 2 (0000 0010)
+<<	        Bitwise left shift	x << 2 = 40 (0010 1000)
+'''
+
+
+# Assignment operators
+
+# Membership operators
+
+# find more on https: // www.programiz.com/python-programming/operators
