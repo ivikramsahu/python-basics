eebc604075e3cddef40bfd62d464e6d4133545d7
Vikram <snipperbytes@gmail.com>
2020-07-06 14:01:40 +0530

    pass similar to comment

---
+++ flow-control/pass.py
@@ -0,0 +1,15 @@
+'''
+pass is just a placeholder for
+functionality to be added later.
+'''
+
+sequence = {'p', 'a', 's', 's'}
+for val in sequence:
+    pass
+
+
+def function(args):
+    pass
+
+
+print(function(123))
